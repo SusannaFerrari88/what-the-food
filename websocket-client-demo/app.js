ac746e5f5c2f5cdfcedd6512a1d6172149efe3f6
Susanna Ferrari <susannaferrari@MacBook-Pro-di-Susanna.local>
2016-04-02 12:43:16 +0200

    websocket server initial commity

---
+++ websocket-client-demo/app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+var router = express.Router();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html', { title: 'Express' });
+});
+
+app.use(router);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+//if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+//}
+
+app.listen(3000, function () {
+  console.log('Ready');
+});
