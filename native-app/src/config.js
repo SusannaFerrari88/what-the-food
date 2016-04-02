3b81529a1b7e65b4cfb9af43a771de4a63d2fd81
Susanna Ferrari <susannaferrari@MacBook-Pro-di-Susanna.local>
2016-04-02 23:33:02 +0200

    cherry pick images
    # Conflicts:
    #	images/nudeln.jpg
    #	images/rice.jpg

--- native-app/src/config.js
+++ native-app/src/config.js
@@ -1,6 +1,6 @@
 export const VIEW_WIDTH = 375
-export const TESTING = false
-export const IP = '192.168.77.134'
+export const TESTING = true
+export const IP = '192.168.77.185'
 export const PORT = '8080'
 
 export const generateTestData = () => ({ jars: [ {
@@ -8,5 +8,5 @@ export const generateTestData = () => ({ jars: [ {
     food      : 'Müsli'
 }, {
     fillAmount: Math.random(),
-    food      : 'Salt'
+    food      : 'Reis'
 } ] })
